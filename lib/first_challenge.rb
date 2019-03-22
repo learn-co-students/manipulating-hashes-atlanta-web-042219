b42818d73ed3725144f31cd043d5590b51ca3ccd
Christopher Kalfas <chriskalfas@yahoo.com>
2019-03-22 20:07:09 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,16 +14,15 @@ def first_challenge
   }
 
   #your code here
-  contacts.each do |person, data|
+  contacts.each do | person, data |
     if person == "Freddy Mercury"
       data.each do |key, value|
         if key == :favorite_icecream_flavors
-          value.delete_if {|flavor| flavor = "strawberry"}
+          value.delete_if {|flavors| flavors == "strawberry"}
         end
       end
     end
-  end 
-
+  end
 
   #remember to return your newly altered contacts hash!
   contacts
